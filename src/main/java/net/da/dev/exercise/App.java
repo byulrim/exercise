32072dd95d2b91db66664b5c3c88e70846d99620
hcshin <yarime@msn.com>
2020-10-04 01:31:51 +0900

    first commit.

---
+++ src/main/java/net/da/dev/exercise/App.java
@@ -0,0 +1,13 @@
+package net.da.dev.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
