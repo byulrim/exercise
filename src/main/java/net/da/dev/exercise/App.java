3db50c3cb47caa08fe923f0ba1892aec0b2657b1
hcshin <yarime@msn.com>
2020-10-08 02:33:46 +0900

    Code modification to App.java

--- src/main/java/net/da/dev/exercise/App.java
+++ src/main/java/net/da/dev/exercise/App.java
@@ -1,13 +1,17 @@
 package net.da.dev.exercise;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		new App().printMessage("byulrim");
+	}
+
+	public String getMessage(String name) {
+		return "Hi, " + name + ". Welcome to Maven World!";
+	}
+
+	public void printMessage(String name) {
+		System.out.println("\n***** App class *****\n");
+		System.out.println(this.getMessage(name));
+		System.out.println("\n***** App class *****\n");
+	}
 }
