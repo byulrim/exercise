a5399e3d092fb8b83483fadd341ba247fe4a2d3c
hcshin <yarime@msn.com>
2020-10-15 00:27:34 +0900

    Modified Errors.

--- src/test/java/net/da/dev/exercise/AppTest.java
+++ src/test/java/net/da/dev/exercise/AppTest.java
@@ -5,9 +5,11 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class AppTest extends TestCase {
+	private App app = null;
 
 	public AppTest(String testName) {
 		super(testName);
+		app = new App();
 	}
 
 	public static Test suite() {
@@ -15,6 +17,12 @@ public class AppTest extends TestCase {
 	}
 
 	public void testApp() {
-		assertTrue(true);
+		assertNotNull(app);
+	}
+
+	public void testGetMessage() {
+		String name = "test";
+		String msg = "Hi, " + name + ". Welcome to Maven World!";
+		assertEquals(app.getMessage("test"), msg);
 	}
 }
